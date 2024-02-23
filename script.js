a2e8ff3db1ec036d6a5ad09dbdc601577838f922
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2024-02-22 23:17:56 -0300

    atualização

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var ativo = document.querySelector('.login')
+var ativado = document.querySelector('.login')
 var desativado = document.querySelector('.signup')
+var h1Signup = document.querySelector('.signup h1')
+var h1Login = document.querySelector('.login h1')
 
-
-ativo.addEventListener('click', () => {
-    ativo.style.top = '15%'
-    ativo.style.transition = 'all 0.6s'
+ativado.addEventListener('click', () => {
+    ativado.style.top = '15%'
+    ativado.style.transition = 'all 0.6s'
+    h1Signup.style.color = 'rgba(255, 255, 255, 0.548)'
+    h1Login.style.color = 'rgba(0, 0, 0, 0.578)'
+    desativado.style.cursor = 'pointer'
+    ativado.style.cursor = 'auto'
 });
 
 desativado.addEventListener('click', () => {
-    ativo.style.top = '87%'
+    ativado.style.top = '87%'
+    h1Signup.style.color = 'white'
+    h1Login.style.color = 'rgb(174, 168, 168)'
+    ativado.style.cursor = 'pointer'
+    desativado.style.cursor = 'auto'
 });
 
